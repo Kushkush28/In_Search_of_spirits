e69a7ef1866048ea9fdab1efc87c8aeaec61087b
kush <kushal.sithlu28@gmail.com>
2022-10-17 15:39:51 +0200

    flying enemy

---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public int maxHealth=100;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage( int dmg) 
+    { 
+    
+    
+    }
+}
